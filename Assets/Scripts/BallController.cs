bd46b9b2c82c3471d90d213af36965b75098ef1e
Husni <mohhusnimubaraq@gmail.com>
2019-11-07 15:53:22 +0700

    first
    First

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public int force;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        Vector2 arah = new Vector2(2,0).normalized;
+        rigidbody.AddForce(arah*force);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.name == "tepiKanan")
+        {
+            resetBall();
+            Vector2 arah = new Vector2(2,0).normalized;
+            rigidbody.AddForce(arah*force);
+        }
+        if(collision.gameObject.name == "tepiKiri")
+        {
+            resetBall();
+            Vector2 arah = new Vector2(-2,0).normalized;
+            rigidbody.AddForce(arah*force);
+        }
+        if(collision.gameObject.name == "paddle1" || collision.gameObject.name == "paddle2")
+        {
+            float sudut = (transform.position.y - collision.transform.position.y) *5f;
+            Vector2 arah = new Vector2(rigidbody.velocity.x,sudut).normalized;
+            rigidbody.velocity = new Vector2(0,0);
+            rigidbody.AddForce(arah*force*2);
+        }
+    }
+
+
+    void resetBall()
+    {
+        transform.localPosition = new Vector2(0,0);
+        rigidbody.velocity = new Vector2(0,0);
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float kecepatan;
+    public string axis;
+
+    public float batasAtas;
+    public float batasBawah;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gerak = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
+        float nextPosition = transform.position.y + gerak;
+
+        if(nextPosition>batasAtas){
+            gerak = 0;
+        }
+        if(nextPosition<batasBawah){
+            gerak = 0;
+        }
+        //nilai x,y,z
+        transform.Translate(0,gerak,0);
+        
+    }
+}
